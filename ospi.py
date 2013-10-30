2cc83847101f86c6a4e31a1e29a9e76d8931c01a
Dan-in-CA <dkimberling@att.net>
2013-10-29 19:08:14 -0700

    Station options save settings.
    "Ignnore rain" and 'Activate mastre" now save settings. hidden fields in
    the form didn't have names.

--- ospi.py
+++ ospi.py
@@ -642,7 +642,8 @@ class change_options:
             ln = len(nlst)
             nlst.pop()
             for i in range((incr*8)+1):
-                nlst.append("'S"+('%d'%(i+ln)).zfill(2)+"'")
+#                nlst.append("'S"+('%d'%(i+ln)).zfill(2)+"'")
+                nlst.append("'S"+('%d'%(i+ln))+"'")
             nstr = '['+','.join(nlst)
             nstr = nstr.replace("', ", "',")+"]"
             save('snames', nstr)            
