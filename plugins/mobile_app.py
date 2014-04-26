efe021ec4bd5342983ad643617937624d5e91d3b
Samer Albahra <salbahra@gmail.com>
2014-04-26 12:48:14 -0500

    Add plugin support from newer branch into 1.9.0 branch

---
+++ plugins/mobile_app.py
@@ -0,0 +1,58 @@
+import web, json, re
+import ast
+import gv # Gain access to ospi's settings
+from urls import urls # Gain access to ospi's URL list
+
+##############
+## New URLs ##
+
+urls.extend(['/jo', 'plugins.mobile_app.options', '/jc', 'plugins.mobile_app.cur_settings', '/js', 'plugins.mobile_app.station_state','/jp', 'plugins.mobile_app.program_info', '/jn', 'plugins.mobile_app.station_info'])
+
+#######################
+## Class definitions ##
+
+class options: # /jo
+    """Returns device options as json."""
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+        jopts = {"fwv":'1.8.3-OSPi',"tz":gv.sd['tz'], "ext":gv.sd['nbrd']-1,"seq":gv.sd['seq'],"sdt":gv.sd['sdt'],"mas":gv.sd['mas'],"mton":gv.sd['mton'],"mtof":gv.sd['mtoff'],"urs":gv.sd['urs'],"rso":gv.sd['rst'],"wl":gv.sd['wl'],"ipas":gv.sd['ipas'],"reset":gv.sd['rbt']}
+        return json.dumps(jopts)
+    
+class cur_settings: # /jc
+    """Returns current settings as json."""
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+        jsettings = {"devt":gv.now,"nbrd":gv.sd['nbrd'],"en":gv.sd['en'],"rd":gv.sd['rd'],"rs":gv.sd['rs'],"mm":gv.sd['mm'],"rdst":gv.sd['rdst'],"loc":gv.sd['loc'],"sbits":gv.sbits,"ps":gv.ps,"lrun":gv.lrun}
+        return json.dumps(jsettings)    
+    
+class station_state: # /js
+    """Returns station status and total number of stations as json."""
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+        jstate = {"sn":gv.srvals, "nstations":gv.sd['nst']}
+        return json.dumps(jstate)
+    
+
+class program_info: # /jp
+    """Returns program data as json."""
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+        jpinfo = {"nprogs":gv.sd['nprogs']-1,"nboards":gv.sd['nbrd'],"mnp":gv.sd['mnp'],"pd":gv.pd}
+        return json.dumps(jpinfo)
+    
+class station_info: # /jn
+    """Returns station information as json."""
+    def GET(self):
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+        f = open('./data/snames.txt', 'r')
+        names = f.read()
+        f.close()
+#        nlst = ast.literal_eval(names) # Convert names var to string (alternative method)
+        nlst = re.findall('[\'|"](.*?)[\'|"]', names) # Convert names var to string
+        jpinfo = {"snames":nlst,"masop":gv.sd['mo'],"maxlen":gv.sd['snlen']}
+        return json.dumps(jpinfo)
