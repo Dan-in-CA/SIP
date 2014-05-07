4c5d8a4ec2266db9b1cfdd0e6f4d03455a80b968
scottshaffer <scottsh@gmail.com>
2014-05-07 11:14:39 -0500

    made station number logged 1-based instead of 0-based

--- ospi.py
+++ ospi.py
@@ -93,7 +93,7 @@ def log_run():
             pgr = 'Manual'
         else:
             pgr = str(gv.lrun[1])
-        datastr = (pgr +', '+str(gv.lrun[0])+', '+str(zones[gv.lrun[0]])+', '+str(gv.lrun[2]/60)+'m'+str(gv.lrun[2]%60)+
+        datastr = (pgr +', '+str(gv.lrun[0]+1)+', '+str(zones[gv.lrun[0]])+', '+str(gv.lrun[2]/60)+'m'+str(gv.lrun[2]%60)+
                    's, '+time.strftime("%H:%M:%S, %a. %d %b %Y", time.gmtime(gv.now))+'\n')
         f = open('./static/log/water_log.csv', 'r')
         log = f.readlines()
