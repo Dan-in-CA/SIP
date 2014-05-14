21c468f5a1e46c2021cb1b75b7badabbc771d445
Dan-in-CA <dkimberling@att.net>
2014-05-14 14:16:41 -0700

    Add plugin files.

---
+++ plugins/relay.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from gpio_pins import *
+from urls import *
+import web, time
+
+urls.extend(['/tr', 'plugins.relay.toggle_relay']) # Add a new url for this plugin.
+
+class toggle_relay:
+    """Test relay by turning it on for a short time, then off."""
+    def GET(self):
+        try:
+            GPIO.output(pin_relay, GPIO.HIGH) # turn relay on
+            time.sleep(3)
+            GPIO.output(pin_relay, GPIO.LOW) #Turn relay off
+        except:
+            pass
+        raise web.seeother('/') # return to home page
