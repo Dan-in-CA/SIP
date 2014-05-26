ad24fc6b33f039821da1c7a6a01c4a1f4da7a00f
Samer Albahra <salbahra@gmail.com>
2014-05-26 17:20:59 -0500

    Mobile Plugin: Support log output and fix program output

--- plugins/mobile_app.py
+++ plugins/mobile_app.py
@@ -1,12 +1,12 @@
 import web, json, re
-import ast
+import ast, time, datetime, string
 import gv # Gain access to ospi's settings
 from urls import urls # Gain access to ospi's URL list
 
 ##############
 ## New URLs ##
 
-urls.extend(['/jo', 'plugins.mobile_app.options', '/jc', 'plugins.mobile_app.cur_settings', '/js', 'plugins.mobile_app.station_state','/jp', 'plugins.mobile_app.program_info', '/jn', 'plugins.mobile_app.station_info'])
+urls.extend(['/jo', 'plugins.mobile_app.options', '/jc', 'plugins.mobile_app.cur_settings', '/js', 'plugins.mobile_app.station_state','/jp', 'plugins.mobile_app.program_info', '/jn', 'plugins.mobile_app.station_info', '/jl', 'plugins.mobile_app.get_logs'])
 
 #######################
 ## Class definitions ##
@@ -39,9 +39,17 @@ class station_state: # /js
 class program_info: # /jp
     """Returns program data as json."""
     def GET(self):
+        lpd = [] # Local program data
+        dse = int((time.time()+((gv.sd['tz']/4)-12)*3600)/86400) # days since epoch
+        for p in gv.pd:
+            op = p[:] # Make local copy of each program
+            if op[1] >= 128 and op[2] > 1:
+                rel_rem = (((op[1]-128) + op[2])-(dse%op[2]))%op[2]
+                op[1] = rel_rem + 128
+            lpd.append(op)
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Content-Type', 'application/json')
-        jpinfo = {"nprogs":gv.sd['nprogs']-1,"nboards":gv.sd['nbrd'],"mnp":gv.sd['mnp'],"pd":gv.pd}
+        jpinfo = {"nprogs":gv.sd['nprogs']-1,"nboards":gv.sd['nbrd'],"mnp":gv.sd['mnp'], 'pd': lpd}
         return json.dumps(jpinfo)
 
 class station_info: # /jn
@@ -56,3 +64,44 @@ class station_info: # /jn
         nlst = re.findall('[\'|"](.*?)[\'|"]', names) # Convert names var to string
         jpinfo = {"snames":nlst,"ignore_rain":gv.sd['ir'],"masop":gv.sd['mo'],"maxlen":gv.sd['snlen']}
         return json.dumps(jpinfo)
+
+class get_logs: # /jl
+    """Returns log information for specified date range."""
+    def GET(self):
+        records = self.read_log()
+        data = []
+        qdict = web.input()
+
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+
+        if not(qdict.has_key('start')) or not(qdict.has_key('end')):
+            return []
+
+        for r in records:
+            event = json.loads(r)
+            date = time.mktime(datetime.datetime.strptime(event["date"], "%Y-%m-%d").timetuple())
+            if int(qdict["start"]) <= int(date) <= int(qdict["end"]):
+                pid = event["program"]
+                if (pid == "Run-once"):
+                    pid = 98
+                if (pid == "Manual"):
+                    pid = 99
+
+                pid = int(pid)
+                station = int(event["station"])
+                duration = int(string.split(event["duration"],":")[0])
+                timestamp = int(time.mktime(datetime.datetime.strptime(event["date"] + " " + event["start"], "%Y-%m-%d %H:%M:%S").timetuple()))
+
+                data.append([pid,station,duration,timestamp])
+
+        return json.dumps(data)
+
+    def read_log(self):
+        try:
+            logf = open('./data/log.json')
+            records = logf.readlines()
+            logf.close()
+            return records
+        except IOError:
+            return []
