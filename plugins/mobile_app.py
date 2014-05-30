bb636ee3c87aec7e428e8320afc9e975196601d3
Dan-in-CA <dkimberling@att.net>
2014-05-30 10:49:15 -0700

    update from origin

--- plugins/mobile_app.py
+++ plugins/mobile_app.py
@@ -1,12 +1,12 @@
 import web, json, re
-import ast
-import gv, time # Gain access to ospi's settings
+import ast, time, datetime, string
+import gv # Gain access to ospi's settings
 from urls import urls # Gain access to ospi's URL list
 
 ##############
 ## New URLs ##
 
-urls.extend(['/jo', 'plugins.mobile_app.options', '/jc', 'plugins.mobile_app.cur_settings', '/js', 'plugins.mobile_app.station_state','/jp', 'plugins.mobile_app.program_info', '/jn', 'plugins.mobile_app.station_info'])
+urls.extend(['/jo', 'plugins.mobile_app.options', '/jc', 'plugins.mobile_app.cur_settings', '/js', 'plugins.mobile_app.station_state','/jp', 'plugins.mobile_app.program_info', '/jn', 'plugins.mobile_app.station_info', '/jl', 'plugins.mobile_app.get_logs'])
 
 #######################
 ## Class definitions ##
@@ -16,7 +16,7 @@ class options: # /jo
     def GET(self):
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Content-Type', 'application/json')
-        jopts = {"fwv":'1.8.3-OSPi',"tz":gv.sd['tz'], "ext":gv.sd['nbrd']-1,"seq":gv.sd['seq'],"sdt":gv.sd['sdt'],"mas":gv.sd['mas'],"mton":gv.sd['mton'],"mtof":gv.sd['mtoff'],"urs":gv.sd['urs'],"rso":gv.sd['rst'],"wl":gv.sd['wl'],"ipas":gv.sd['ipas'],"reset":gv.sd['rbt']}
+        jopts = {"fwv":'1.9.0-OSPi',"tz":gv.sd['tz'], "ext":gv.sd['nbrd']-1,"seq":gv.sd['seq'],"sdt":gv.sd['sdt'],"mas":gv.sd['mas'],"mton":gv.sd['mton'],"mtof":gv.sd['mtoff'],"urs":gv.sd['urs'],"rso":gv.sd['rst'],"wl":gv.sd['wl'],"ipas":gv.sd['ipas'],"reset":gv.sd['rbt']}
         return json.dumps(jopts)
 
 class cur_settings: # /jc
@@ -35,23 +35,21 @@ class station_state: # /js
         jstate = {"sn":gv.srvals, "nstations":gv.sd['nst']}
         return json.dumps(jstate)
 
+
 class program_info: # /jp
     """Returns program data as json."""
     def GET(self):
         lpd = [] # Local program data
-        dse = int(gv.now/86400) # days since epoch
+        dse = int((time.time()+((gv.sd['tz']/4)-12)*3600)/86400) # days since epoch
         for p in gv.pd:
             op = p[:] # Make local copy of each program
             if op[1] >= 128 and op[2] > 1:
-                rel_rem = (((op[1]-128) + op[2])-(dse % op[2])) % op[2] # Convert absolute days to relative remaining (rel_rem) days
-                op[1] = rel_rem + 128 # Update from saved value based on current date
+                rel_rem = (((op[1]-128) + op[2])-(dse%op[2]))%op[2]
+                op[1] = rel_rem + 128
             lpd.append(op)
-        progstr = ''
-        for i, pro in enumerate(lpd): #gets both index and object
-            progstr += 'pd['+str(i)+']='+str(pro).replace(' ', '')+';'
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Content-Type', 'application/json')
-        jpinfo = {"nprogs":gv.sd['nprogs']-1,"nboards":gv.sd['nbrd'],"mnp":gv.sd['mnp'], 'pd': progstr}
+        jpinfo = {"nprogs":gv.sd['nprogs']-1,"nboards":gv.sd['nbrd'],"mnp":gv.sd['mnp'], 'pd': lpd}
         return json.dumps(jpinfo)
 
 class station_info: # /jn
@@ -59,10 +57,64 @@ class station_info: # /jn
     def GET(self):
         web.header('Access-Control-Allow-Origin', '*')
         web.header('Content-Type', 'application/json')
-        f = open('./data/snames.txt', 'r')
-        names = f.read()
-        f.close()
-#        nlst = ast.literal_eval(names) # Convert names var to string (alternative method)
+        names = data('snames')
         nlst = re.findall('[\'|"](.*?)[\'|"]', names) # Convert names var to string
         jpinfo = {"snames":nlst,"ignore_rain":gv.sd['ir'],"masop":gv.sd['mo'],"maxlen":gv.sd['snlen']}
         return json.dumps(jpinfo)
+
+class get_logs: # /jl
+    """Returns log information for specified date range."""
+    def GET(self):
+        records = self.read_log()
+        data = []
+        qdict = web.input()
+
+        web.header('Access-Control-Allow-Origin', '*')
+        web.header('Content-Type', 'application/json')
+
+        if not(qdict.has_key('start')) or not(qdict.has_key('end')):
+            return []
+
+        for r in records:
+            event = json.loads(r)
+            date = time.mktime(datetime.datetime.strptime(event["date"], "%Y-%m-%d").timetuple())
+            if int(qdict["start"]) <= int(date) <= int(qdict["end"]):
+                pid = event["program"]
+                if (pid == "Run-once"):
+                    pid = 98
+                if (pid == "Manual"):
+                    pid = 99
+
+                pid = int(pid)
+                station = int(event["station"])
+                duration = int(string.split(event["duration"],":")[0])
+                timestamp = int(time.mktime(datetime.datetime.strptime(event["date"] + " " + event["start"], "%Y-%m-%d %H:%M:%S").timetuple()))
+
+                data.append([pid,station,duration,timestamp])
+
+        return json.dumps(data)
+
+    def read_log(self):
+        try:
+            logf = open('./data/log.json')
+            records = logf.readlines()
+            logf.close()
+            return records
+        except IOError:
+            return []
+
+def data(dataf):
+    """Return contents of requested text file as string or create file if a missing config file."""
+    try:
+        f = open('./data/'+dataf+'.txt', 'r')
+        data = f.read()
+        f.close()
+    except IOError:
+        if dataf == 'snames': ## A config file -- return defaults and create file if not found. ##
+            data = "['S1','S2','S3','S4','S5','S6','S7','S8',]"
+            f = open('./data/'+dataf+'.txt', 'w')
+            f.write(data)
+            f.close()
+        else:
+            return None
+    return data
