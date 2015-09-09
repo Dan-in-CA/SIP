2c35cf068856cd97d7d973ab67e30ed99c11e635
Dan <dkimberling59@gmail.com>
2015-09-09 15:50:11 -0700

    Fix duration bug
    Duration was not defined in "if" block when station was set to ignore water causing an error.

--- sip.py
+++ sip.py
@@ -49,7 +49,8 @@ def timing_loop():
 
             				# station duration condionally scaled by "water level"
                                 if gv.sd['iw'][b] & 1 << s:
-                                    duration_adj = 1.0 
+                                    duration_adj = 1.0
+                                    duration = p[6] 
                                 else:
                                     duration_adj = gv.sd['wl'] / 100 * extra_adjustment
                                     duration = p[6] * duration_adj
