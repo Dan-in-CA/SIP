e59b807adf698fbf36957afe82e566d4641d76d5
Dan <dkimberling@att.net>
2019-11-09 13:19:16 -0800

    Add message for running under Python3.

--- reprlib/__init__.py
+++ reprlib/__init__.py
@@ -1,9 +1,24 @@
+# from __future__ import absolute_import
+# from future.utils import PY3
+# 
+# if PY3:
+#     from reprlib import *
+# else:
+#     __future_module__ = True
+#     from repr import *
+
+
 from __future__ import absolute_import
 import sys
-
+ 
 if sys.version_info[0] < 3:
     from repr import *
 else:
-    raise ImportError('This package should not be accessible on Python 3. '
-                      'Either you are trying to run from the python-future src folder '
-                      'or your installation of python-future is corrupted.')
+    raise ImportError('\n To run SIP under Python 3 '
+                      ' Please rename the folder reprlib. \n'
+                      ' In the SIP directory, use the command: \n'
+                      ' mv reprlib reprlib-bak ')
+    
+#     raise ImportError('This package should not be accessible on Python 3. '
+#                       'Either you are trying to run from the python-future src folder '
+#                       'or your installation of python-future is corrupted.')
