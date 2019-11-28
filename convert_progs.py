0c0b907c4ec31567679fa90e6e0667724a0764d1
Dan-in-CA <dkimberling@att.net>
2019-11-28 09:34:48 -0800

    Fix convert_progs.py interval bug.

--- convert_progs.py
+++ convert_progs.py
@@ -6,14 +6,7 @@ from __future__ import division
 from future.builtins import range
 import json
 import gv
-import time
-from calendar import timegm
 
-
-gv.now = timegm(
-    time.localtime()
-)
-dse = int(gv.now // 86400)
 
 prog_keys = [
     u"enabled",
@@ -36,12 +29,10 @@ def convert():
         for j in range(len(prog_keys)): # for each element in new format (j)
             new_prog[prog_keys[j]] = old_data[i][j]
         if gv.sd[u'idd']:
-#         if type(old_data[-1]) == list: # If the last item in old data is a list
             new_prog[u"duration_sec"] = old_data[i].pop() # copy list from old data
         else:
             new_prog[u"duration_sec"] = [old_data[i][6]]
         new_prog[u"enabled"] = old_data[i][0]    
-#         new_prog[u"station_mask"] = old_data[i][7] #### needs special treatment for expansion boards
         if type(new_prog[u"station_mask"]) == int:
                 new_prog[u"station_mask"] =[old_data[i][7]]
         new_prog[u"name"] = u"Unnamed" # Not in old format
@@ -51,15 +42,14 @@ def convert():
         if not new_prog[u"day_mask"] & 128:  #  if high bit not set
             new_prog[u"type"] = u"alldays"
         elif (
-            new_prog[u"day_mask"] & 128
+            new_prog[u"day_mask"] & 128  #  if high bit is set
             and new_prog[u"interval_base_day"] < 2  # low bit is 0 or 1
         ):
             days = [u"evendays", u"odddays"]
             new_prog[u"type"] = days[new_prog[u"interval_base_day"]]
-            new_prog[u"day_mask"] &= ~128  #  clear high bit
         elif new_prog[u"interval_base_day"] >= 2:
             new_prog[u"type"] = u"interval"
-            ref = dse + new_prog[u"day_mask"]
+            new_prog[u"day_mask"] &= ~128  #  clear high bit
         if ((new_prog[u"start_min"] + (sum(new_prog[u"duration_sec"]) / 60)) >= new_prog[u"stop_min"]):
             new_prog[u"cycle_min"] = 0
         if old_data[i][5]>= new_prog[u"stop_min"]:
@@ -71,5 +61,5 @@ def convert():
 
 if __name__ == "__main__":
     new_data = convert()
-    with open("./data/programData.json", "w") as pf:
+    with open(u"./data/programData.json", u"w") as pf:
         json.dump(new_data, pf, indent=4, sort_keys=True)
